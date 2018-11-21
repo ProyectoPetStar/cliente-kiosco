bf2b3a830dd0c5904d00c9180298ad59a608d321
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-11-20 18:12:42 -0600

    Inicio formulario de kioscos

--- src/app/administracion/form-kioscos/form-kioscos.component.ts
+++ src/app/administracion/form-kioscos/form-kioscos.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormKioscosService } from './form-kioscos.service';
 
 @Component({
   selector: 'app-form-kioscos',
   templateUrl: './form-kioscos.component.html',
-  styleUrls: ['./form-kioscos.component.scss']
+  styleUrls: ['./form-kioscos.component.scss'],
+  providers: [ FormKioscosService ]
 })
 export class FormKioscosComponent implements OnInit {
 
-  constructor() { }
+  public loading: boolean;
+  public notValid: boolean;
+  public action: string;
+
+  constructor(
+    private auth: AuthService,
+    private service: FormKioscosService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.loading = true;
   }
 
 }
--- src/app/administracion/form-kioscos/form-kioscos.service.ts
+++ src/app/administracion/form-kioscos/form-kioscos.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { BASE_URL_SERVICE } from '../../constants';
+import { Kiosco } from '../../models/kiosco';
 
 @Injectable()
 export class FormKioscosService {
 
-  constructor() { }
+  private URL = BASE_URL_SERVICE + '/Kiosco';
+
+  constructor(private http: HttpClient) { }
+
+  getKioscoById(id_usuario: number, id_kiosko: number): Observable<any> {
+    return this.http.get<any>(this.URL + '?action=getKioscoById&id_usuario=' + id_usuario + '&id_kiosko=' + id_kiosko);
+  }
+  
 
 }
