aa19b4559084e11cdda087153732c46bf2e8fa09
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-11-27 13:49:59 -0600

    Change background

--- src/app/kiosco/portal/portal.component.ts
+++ src/app/kiosco/portal/portal.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const $: any;
+declare const WOW: any;
+
+
 @Component({
   selector: 'app-portal',
   templateUrl: './portal.component.html',
-  styleUrls: ['./portal.component.scss']
+  styleUrls: [
+    './portal.component.scss',
+    '../../../assets/css/style.css',
+    '../../../assets/css/animate.min.css'
+  ]
 })
 export class PortalComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      $('.section-labs').hide();
+      new WOW().init();
+    }, 50);
   }
 
 }
