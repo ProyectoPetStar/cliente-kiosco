b0296fab2d371eb686bc9d0fdfc55a3d701aaeb2
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2019-04-09 18:20:57 -0500

    KIOSCO [ESTABLE v1.0.0]

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
-export const BASE_URL_SERVICE = 'https://kiosco.petstar.mx:444/kiosco';
-export const SOCKET_WS = 'wss://kiosco.petstar.mx:444/kiosco/P3t5t4rWs';
-export const URL_IMAGES = 'https://kiosco.petstar.mx/images_kiosco';
+export const BASE_URL_SERVICE = 'http://localhost:8080/kiosco';
+export const SOCKET_WS = 'ws://localhost:8080/kiosco/P3t5t4rWs';
+export const URL_IMAGES = 'http://localhost/images_kiosco';
 export const API_IP = 'https://api.ipify.org/?format=json';
 // export const BASE_URL_SERVICE = 'http://192.168.16.244:8082/kiosco';
